4ae88a4a22392c75198b0c6b5c6e65316e3dfded
Yarden Eshel <yardenyarden5@gmail.com>
2024-01-27 19:16:30 +0200

    finished

--- Editor2.java
+++ Editor2.java
@@ -10,9 +10,9 @@ import java.awt.Color;
 public class Editor2 {
 
 	public static void main (String[] args){
-		String fileName = args[0];
-		int width = Integer.parseInt(args[1]);
-		int height = Integer.parseInt(args[2]);
+		String fileName = "ironman.ppm";
+		int width = 100;
+		int height = 900;
 		Color[][] imageIn = Runigram.read(fileName);
 		Color[][] scaledImage = Runigram.scaled(imageIn, width, height);
 		// Displays the source image
